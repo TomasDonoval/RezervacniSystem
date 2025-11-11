22139ced82d7e28ae76128c3d3b65feaca98ae06
Tomas Donoval <tomas.donoval13@gmail.com>
2025-11-11 14:27:24 +0100

    Automatická migrace databazé

--- RezervacniSystem/Program.cs
+++ RezervacniSystem/Program.cs
@@ -28,6 +28,12 @@ builder.Services.ConfigureApplicationCookie(options =>
 
 var app = builder.Build();
 
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    db.Database.Migrate();
+}
+
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
